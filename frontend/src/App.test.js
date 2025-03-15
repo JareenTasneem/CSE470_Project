ac457620fb3bd6837113b4f7c8f98205469193e8
JareenTasneem <jareentasneem3@gmail.com>
2025-03-15 23:45:30 +0600

    Update App.test.js

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,8 +1,29 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+// src/App.js
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import TravelPack from "./TravelPack";           // Your landing page
+import TourPackagesList from "./TourPackagesList"; // The list page with search
+import TourPackageDetails from "./TourPackageDetails"; // The detail ("view more") page
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+import Signup from "./Signup";
+import Login from "./Login";
+
+import CustomizePackage from "./CustomizePackage";
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<TravelPack />} />
+        <Route path="/tourPackages" element={<TourPackagesList />} />
+        <Route path="/tourPackages/:id" element={<TourPackageDetails />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
+
+        <Route path="/customize-package" element={<CustomizePackage />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
